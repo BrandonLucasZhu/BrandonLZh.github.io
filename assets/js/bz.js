2868a515a940e5605e465c61d225308a710d9195
Brandon Lucas Zhu <brandonlucaszhu@gmail.com>
2016-11-09 13:54:30 -0500

    changed fade in text to fade out

--- assets/js/bz.js
+++ assets/js/bz.js
@@ -1,17 +1,19 @@
-
-
 $(window).on("load",function() {
   $(window).scroll(function() {
     $(".container").each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top ;
+      var windowBottom = $(window).scrollTop();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectBottom >= windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {
+            $(this).fadeTo(2000,1);
+          }
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {
+          $(this).fadeTo(2000,0);
+          }
       }
     });
   }); $(window).scroll(); //invoke scroll-handler on page-load
